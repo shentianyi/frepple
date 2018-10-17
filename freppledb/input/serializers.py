8b467b33805d0c082011cd8f365b1fdbdcbd3a81
Charlot <7wangsong@gmail.com>
2018-10-17 19:08:01 +0800

    update

--- freppledb/input/serializers.py
+++ freppledb/input/serializers.py
@@ -196,7 +196,7 @@ class LocationdetailAPI(frePPleRetrieveUpdateDestroyAPIView):
     queryset = freppledb.input.models.Location.objects.all()
     serializer_class = LocationSerializer
 
-# CMARL 根据自然键查询/删除
+# CMARK 根据自然键查询/删除
 # TODO　多个自然键不支持
 class LocationdetailNkAPI(frePPleRetrieveUpdateDestroyAPIView):
     # natural key 自然键
