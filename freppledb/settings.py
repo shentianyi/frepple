321244608c235b7070f50d895e8b82e20c0775da
Charlot <7wangsong@gmail.com>
2018-10-31 12:08:22 +0800

    batch operate forecast api

--- freppledb/settings.py
+++ freppledb/settings.py
@@ -224,7 +224,7 @@ CSV_CHARSET = 'utf-8' # locale.getdefaultlocale()[1]
 # If the list contains only a single value, the preferences screen will not
 # display users an option to choose the theme.
 THEMES = [
-  'earth', 'grass', 'lemon', 'odoo', 'openbravo', 'orange', 'snow', 'strawberry', 'water', 'laura'
+   'laura', 'earth', 'grass', 'lemon', 'odoo', 'openbravo', 'orange', 'snow', 'strawberry', 'water'
   ]
 
 # A default user-group to which new users are automatically added
